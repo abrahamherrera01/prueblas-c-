830e86a5401d77109993796ff4a35ac3563ab120
missk08 <34293147+missk08@users.noreply.github.com>
2017-12-05 21:25:32 -0600

    Add files via upload

---
+++ Nueva carpeta/main.cpp
@@ -0,0 +1,14 @@
+//cuenta regresiva del 100 a el 1// 	
+#include "iostream"
+
+using namespace std;
+int main()
+{
+
+	for(int i=100; i > 0; i--)
+	{//Notemos que escribir i-- es similar a escribir i = i - 1
+		cout << i << endl;
+	}
+	system("PAUSE");
+	return 0;
+}

b1969b5d0561e93d75cb683289c8b17ffd7a6493
missk08 <34293147+missk08@users.noreply.github.com>
2017-12-05 21:52:33 -0600

    Add files via upload

---
+++ Nueva carpeta (2)/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{  
+	
+int x=0; 
+      
+for(x+1;x<=100;x++) 
+
+if(x % 2 == 0) 
+{ cout << x << endl; 
+
+}	
+	
+	return 0;
+ }
